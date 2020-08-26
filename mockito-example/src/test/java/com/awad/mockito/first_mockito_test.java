87c5530227ad191dd03000bc08eabebe7862baae
AwadYMohammed <awadyasirawad@gmail.com>
2020-08-26 01:01:17 -0400

    Written a simple test

---
+++ mockito-example/src/test/java/com/awad/mockito/first_mockito_test.java
@@ -0,0 +1,15 @@
+package com.awad.mockito;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class first_mockito_test {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+		//fail("Not yet implemented");
+	}
+
+}
